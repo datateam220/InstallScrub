1a47333525d7bc4ec86e9002981bf33821e19979
datateam220 <nicks@tankolighting.com>
2018-05-16 10:24:21 -0700

    Major bug fixes

---
+++ tests.py
@@ -0,0 +1,21 @@
+'''
+Install Scrub Testing Suite
+'''
+
+'''
+Test Initialization
+ - Copy dataset and reset values
+ - Set values on specific, known records to validate test results later.
+'''
+
+'''
+Session Tests
+ - Initialize a scrub session and verify it exists.
+ - write test lines to log files.
+ - close log files
+ - ensure log files no longer writeable
+'''
+
+'''
+
+'''
